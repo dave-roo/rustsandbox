74b573ec3004e4ad571640c67410d0610a79522b
roodave <roo@posteo.de>
2018-01-30 22:42:43 +0100

    rust formatting day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world")
+    println!("{subject} {verb} {object}",
+                 object="cow",
+                 subject="moon",
+                 verb="jumps over");
 }
