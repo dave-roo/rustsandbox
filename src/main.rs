dd6a0854092f2c5d5b6d872084914b168d4abdfa
roodave <roo@posteo.de>
2018-01-31 22:38:28 +0100

    a further look into Structures and formatting.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,20 @@ fn main() {
 
     println!("My name is {0}, {1} {0}", "Bond", "James");
 
-    #[allow(dead_code)]
+
+    #[derive(Debug)]
     struct Structure(i32);
+
+    #[derive(Debug)]
+    struct Deep(Structure);
+
+    println!("{:?} months in a year.", 12);
+    println!("{1:?} {0:?} is the {actor:?} name.",
+             "Slater",
+             "Christian",
+             actor="actor's");
+
+    println!("Now {:?} will print!", Structure(3));
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
 }
