adb2708ae395899117047da9a0c40f8c678cdbbd
roodave <roo@posteo.de>
2018-01-31 22:41:00 +0100

    using structures and pretty print {:#?}

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8
+}
+
 fn main() {
+
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Pretty print
+    println!("{:#?}", peter);
+
     println!("{} days", 31);
 
     println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
