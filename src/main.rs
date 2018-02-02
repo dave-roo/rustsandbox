f1181efff1d4555351b65c9fa75c68a6f253eb38
roodave <roo@posteo.de>
2018-02-02 07:42:04 +0100

    using ? and a test case for it (generic: List)

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,25 @@
-use std::fmt; 
+use std::fmt;
+struct List(Vec<i32>);
 
-#[derive(Debug)]
-struct MinMax(i64, i64);
-
-impl fmt::Display for MinMax {
+impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.0, self.1)
-    }
-}
 
-#[derive(Debug)]
-struct Point2D {
-    x: f64,
-    y: f64,
-}
+        let vec = &self.0;
 
-impl fmt::Display for Point2D {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "x: {}, y: {}", self.x, self.y)
+        write!(f, "[")?;
+
+        for (count, v) in vec.iter().enumerate() {
+
+            // Use the ? operator, or try!, to return on errors.
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}", v)?;
+        }
+
+        write!(f, "]")
     }
 }
 
 fn main() {
-    let minmax = MinMax(0, 14);
-
-    println!("Compare structures:");
-    println!("Display: {}", minmax);
-    println!("Debug: {:?}", minmax);
-
-    let big_range =   MinMax(-300, 300);
-    let small_range = MinMax(-3, 3);
-
-    println!("The big range is {big} and the small is {small}",
-             small = small_range,
-             big = big_range);
-
-    let point = Point2D { x: 3.3, y: 7.2 };
-
-    println!("Compare points:");
-    println!("Display: {}", point);
-    println!("Debug: {:?}", point);
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 }
