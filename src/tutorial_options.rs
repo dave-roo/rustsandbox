63e041b273298ed3229481a0e6b3bb69afacee5e
dave-roo <35938373+dave-roo@users.noreply.github.com>
2018-02-10 17:31:13 +0100

    learning about Options in Rust
    learning about Options in Rust

---
+++ src/tutorial_options.rs
@@ -0,0 +1,15 @@
+﻿fn divison(x: f64, y: f64) -> Option<f64> {
+	if y = 0.0 {
+		None
+	} else {
+		Some(x/y)
+	}
+}
+
+fn main() {
+	let res = divison(5.0, 7.0);
+	match res {
+		Some(x) -> println!("{}", x),
+		None -> println!("cannot divide by 0"),
+	}
+}
