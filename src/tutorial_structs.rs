26f81d074ad1366d5152ace874ecad54ead85aad
dave-roo <35938373+dave-roo@users.noreply.github.com>
2018-03-10 01:01:13 +1100

    changes to struct example
    changes to struct example

--- src/tutorial_structs.rs
+++ src/tutorial_structs.rs
@@ -1,12 +1,22 @@
 ﻿fn main() {
-    let pair = (2, -2);
+    struct Foo { x: (u32, u32), y: u32 }
 
-    println!("Tell me about {:?}", pair);
-    match pair {
-        (x, y) if x == y => println!("These are twins"),
-        // The ^ `if condition` part is a guard
-        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
-        (x, _) if x % 2 == 1 => println!("The first one is odd"),
-        _ => println!("No correlation..."),
-    }
+    // destructure members of the struct
+    let foo = Foo { x: (1, 2), y: 3 };
+    let Foo { x: (a, b), y } = foo;
+
+    println!("a = {}, b = {},  y = {} ", a, b, y);
+
+    // you can destructure structs and rename the variables,
+    // the order is not important
+
+    let Foo { y: i, x: j } = foo;
+    println!("i = {:?}, j = {:?}", i, j);
+
+    // and you can also ignore some variables:
+    let Foo { y, .. } = foo;
+    println!("y = {}", y);
+
+    // this will give an error: pattern does not mention field `x`
+    // let Foo { y } = foo;
 }
