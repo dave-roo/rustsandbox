161a3cd56c23b6ce0a7e8428ef9c61fdcf485790
dave-roo <35938373+dave-roo@users.noreply.github.com>
2018-03-14 23:28:26 +1100

    implementation types
    implementation types

--- src/generics_good_example.rs
+++ src/generics_good_example.rs
@@ -1,3 +1,22 @@
+struct Val {
+    val: f64
+}
+
+struct GenVal<T>{
+    gen_val: T
+}
+
+// impl of Val
+impl Val {
+    fn value(&self) -> &f64 { &self.val }
+}
+
+// impl of GenVal for a generic type `T`
+impl <T> GenVal<T> {
+    fn value(&self) -> &T { &self.gen_val }
+}
+
+
 
 fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
@@ -17,6 +36,9 @@ struct Point<T> {
 }
 
 fn main() {
+
+    let x = Val { val: 3.0 };
+    let y = GenVal { gen_val: 3i32 };
     let number_list = vec![34, 50, 25, 100, 65];
 
     //let result = largest(&number_list);
@@ -30,4 +52,6 @@ fn main() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
 //  let wontwork = Point { x: 1, y: 4.0 };
+
+    println!("{}, {}", x.value(), y.value());
 }
